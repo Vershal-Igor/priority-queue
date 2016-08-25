f15fa17a9b4a2dd4cbd9cf07450dd6f0b0b63b87
Vershal-Igor <igorvershal@gmail.com>
2016-08-25 11:44:42 +0400

    did Node without SWAP

--- src/node.js
+++ src/node.js
@@ -1,19 +1,43 @@
+"use strict"
 class Node {
-	constructor(data, priority) {
+  constructor(data, priority) {
+	this.data = data;
+    this.priority = priority;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
 
 	}
 
-	appendChild(node) {
+  appendChild(node) {
+	if (!this.left) {
+      this.left = node;
+      node.parent = this;
+    } else if (!this.right) {
+      this.right = node;
+      node.parent = this;
+    }
 
 	}
 
-	removeChild(node) {
+  removeChild(node) {
+	if (this.left && this.left === node) {
+      node.parent = null;
+      this.left = null;
+     } else if (this.right && this.right === node) {
+      node.parent = null;
+      this.right = null;
+     } else {
+      throw new Error("Node isn't a child of this node");
+     }
 
 	}
 
-	remove() {
-
-	}
+  remove() {
+    if (this.parent) {
+      this.parent.removeChild(this);
+    }
+  }
 
 	swapWithParent() {
 		
