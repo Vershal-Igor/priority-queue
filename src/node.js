31bedc654a7b6544f546c44defbbf4486a21dc4a
Vershal-Igor <igorvershal@gmail.com>
2016-08-25 12:35:30 +0400

    added swapWithParent

--- src/node.js
+++ src/node.js
@@ -40,6 +40,77 @@ class Node {
   }
 
 	swapWithParent() {
+    /* Чтобы обновить ссылки parent, left, right не только в самих нодах, которые меняем местами, но
+    и в дочерних (parent) и родительских (left/right) вычищаем все ссылки на эти ноды
+    из "родителей" и "детей", обнулияем ссылки из этих нод, а потом все расставим заново.
+    Для этого:
+     1) скэшируем ссылки на left и right у всех задействованных в изменениях нодах
+     2) уберем в нодах, которые мы меняем местами ссылку на parent - посредством вызова метода removeChild у родителей
+     3) очистим собственные свойства left/right у нод, которые меняем местами
+     4) проставим ссылки на parent в наших нодах заново, посредством вызова метода appendChild у , и  в  (если он существует), а parent - в child.
+        а) parent.parent (передав child)
+        б) child (передав parent) - т.е. по сути меняя местами ноды
+     5) проставим ссылки на left и right, которые сохранили в кэше. Для child - ссылки из parent, для parent - из child
+     */
+		if(this.parent) {
+			var childNode = this;
+			var parentNode = this.parent;
+			var grandNode = parentNode.parent;
+
+			parentNode.removeChild(childNode);
+
+            //Кэшируем линки на left/right для childNode и для parentNode
+			var tmp_child = {
+				left: childNode.left,
+				right: childNode.right
+			};
+
+			var tmp_parent = {
+				left: parentNode.left,
+				right: parentNode.right          
+			};
+			console.log(tmp_child);
+
+            // "Смотрим" на уровень выше - если у parentNode был свой Parent, надо в нём сменить указатель на childNode.
+			if(grandNode){
+              // Добавляем childNode в parentNode.parent, если он существует.  
+				grandNode.removeChild(parentNode);
+				grandNode.appendChild(childNode);
+			}
+
+			// Удаляем линки на left и right в parent и child.
+			if (parentNode.left){
+				parentNode.removeChild(parentNode.left);
+			}
+			if (parentNode.right){
+				parentNode.removeChild(parentNode.right);
+			}
+			if (childNode.left){
+				childNode.removeChild(childNode.left);
+			}
+			if(childNode.right){
+				childNode.removeChild(childNode.right);
+			}
+
+			 // Добавляем parent в child.
+			 childNode.appendChild(parentNode);
+
+			 // Сеттим новые линки на left и right в parent и child.
+			 if (tmp_child.left){
+			 	parentNode.appendChild(tmp_child.left);
+			 }
+			 if(tmp_child.right){
+			 	parentNode.appendChild(tmp_child.right);
+			 }
+
+			 if(tmp_parent.left){
+			 	childNode.appendChild(tmp_parent.left);
+			 }
+			 if(tmp_parent.right){
+			 	childNode.appendChild(tmp_parent.right);
+			 }
+
+		}
 		
 	}
 }
