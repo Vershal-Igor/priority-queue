a7a602d61118ccf6bb7ae7fbf9a9518c30794a56
Vershal-Igor <igorvershal@gmail.com>
2016-08-28 20:47:50 +0400

    max-heap

--- src/max-heap.js
+++ src/max-heap.js
@@ -1,3 +1,4 @@
+"use strict"
 const Node = require('./node');
 
 class MaxHeap {
@@ -16,16 +17,37 @@ class MaxHeap {
   }
 
 	pop() {
-		
-	}
+    if (this.parentNodes.length !== 0) {
+      var detached = this.detachRoot();
+      this.restoreRootFromLastInsertedNode(detached);
+      this.shiftNodeDown(this.root);
+      return this.root.data;
+    }
+  }
 
-	detachRoot() {
-		
-	}
+  detachRoot() {
+    var lastDetachedRoot = this.root;
+    this.root = null;
+    // removes root from parentNodes
+    var rootIndex = this.parentNodes.indexOf(lastDetachedRoot);
+    if (rootIndex !== -1) {
+      // т.к. root будет всегда первым
+      this.parentNodes = this.parentNodes.slice(1);
+    }
+    return lastDetachedRoot;
+  }
 
 	restoreRootFromLastInsertedNode(detached) {
-		
-	}
+    this.lastInserted.remove();
+    if (detached.left !== null) {
+      this.lastInserted.appendChild(detached.left)
+    }
+    if (detached.right !== null) {
+      this.lastInserted.appendChild(detached.right)
+    }
+    this.root = this.lastInserted;
+    this.afterShift();
+  }
 
 	size() {
 		
@@ -42,6 +64,34 @@ class MaxHeap {
 	insertNode(node) {
 		
 	}
+	
+	afterShift() {
+    this.lastInserted = null;
+    this.heapSize = 0;
+    var newParentArray = [];
+    var nodesForCheck = [this.root];
+
+    while (nodesForCheck.length > 0) {
+      var newCheckArray = [];
+      for (var i = 0; i < nodesForCheck.length; i++) {
+        if (nodesForCheck[i].left === null && nodesForCheck[i].right === null) {
+          newParentArray.push(nodesForCheck[i]);
+        } else if (nodesForCheck[i].left === null || nodesForCheck[i].right === null) {
+          newParentArray.push(nodesForCheck[i]);
+        }
+        if (nodesForCheck[i].left !== null) {
+          newCheckArray.push(nodesForCheck[i].left);
+        }
+        if (nodesForCheck[i].right !== null) {
+          newCheckArray.push(nodesForCheck[i].right);
+        }
+        this.lastInserted = nodesForCheck[i];
+        this.heapSize++;
+      }
+      nodesForCheck = newCheckArray;
+    }
+    this.parentNodes = newParentArray;
+  }
 
 	shiftNodeUp(node) {
     if (node.parent !== null) {
